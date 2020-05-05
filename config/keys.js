e87b336c19fbb20f689378f0fa86652d9ad9d6df
Lihemen <lihemen@yahoo.com>
2020-05-05 16:11:39 +0100

    Login and registeration using passport and mongoDb

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+	MongoURI: 'mongodb://localhost:27017/merkthev'
+};
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Merkthev:<password>@merkthev-apo8z.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
